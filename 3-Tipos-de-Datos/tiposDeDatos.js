07e59ed5097f9f88e354076153e14bcea74f713a
ArthurPain <138066629+OPJMendez@users.noreply.github.com>
2023-08-17 21:03:35 -0500

    feat

---
+++ 3-Tipos-de-Datos/tiposDeDatos.js
@@ -0,0 +1,19 @@
+/*
+
+Tipos de datos que podemos guardar en las variables:
+
+-> string : Cadena de texto.
+-> number : Números.
+-> boolean : Booleano (true o false).
+-> object : Objeto.
+-> function : Funciones.
+
+-> null : Valor Nulo.
+-> undefined : Valor sin definir (es el valor por defecto).
+
+*/
+let cadenaDeTexto = "Texto y 'más texto'";
+let numeros = 100;
+let decimales = 14.13;
+let recibido = true;
+recibido = false;
