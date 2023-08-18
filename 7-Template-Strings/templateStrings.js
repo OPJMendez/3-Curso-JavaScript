14a4fcbef91e53c8e12a69b2a801749e9e150e17
ArthurPain <138066629+OPJMendez@users.noreply.github.com>
2023-08-18 11:25:14 -0500

    feat

---
+++ 7-Template-Strings/templateStrings.js
@@ -0,0 +1,11 @@
+/*
+Nos permiten trabajar con variables dentro de cadenas de texto.
+*/
+//  Usamos el acento grave o "backtick"
+const userName = "Carlos Arturo";
+const age = 27;
+const country = "Holanda";
+//const userInfo = `El nombre de la persona es ${name}, su edad es ${age} y vive en ${country}`;
+console.log(
+  `El nombre de la persona es ${userName}, su edad es ${age} y vive en ${country}`
+);
