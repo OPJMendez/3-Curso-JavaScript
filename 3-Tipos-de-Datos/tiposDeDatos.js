5031a0ca0f41188f1badf6d4a84df3b425c87ab2
ArthurPain <138066629+OPJMendez@users.noreply.github.com>
2023-08-18 08:38:23 -0500

    feat

--- 3-Tipos-de-Datos/tiposDeDatos.js
+++ 3-Tipos-de-Datos/tiposDeDatos.js
@@ -15,5 +15,51 @@ Tipos de datos que podemos guardar en las variables:
 let cadenaDeTexto = "Texto y 'más texto'";
 let numeros = 100;
 let decimales = 14.13;
-let recibido = true;
-recibido = false;
+let usuarioConectado = false;
+
+// Arrays - Arreglos
+/*
+Nos permiten guardar multiples valores de distintos tipos de datos, incluso arreglos dentro de arreglos.
+*/
+
+const nameList = ["Alicia", "JJ", "Prince", 21];
+//console.log(nameList);
+
+// Objetos
+/*
+Nos permiten guardar datos en parejas, (Propiedad y valor)
+
+Ejemplo:
+*/
+const person = {
+  name: "Carlos",
+  age: 33,
+  carro: {
+    marca: "Ferrari",
+    color: "Negro",
+  },
+};
+//console.log(person);
+/*
+Podemos acceder a distintos valores usando .
+*/
+//console.log(person.age);
+//console.log(person.carro);
+
+// Funciones
+function printHello() {
+  console.log("Hello");
+}
+//printHello();
+
+// Null
+/*
+Normalmente se usa cuando queremos especificar que un valor sea nulo.
+*/
+let nullValue = null;
+
+// Undefined
+/*
+Se usa para indicarnos que un valor no esta definido.
+*/
+let undefinedValue = undefined;
