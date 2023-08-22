642ff7d4512bfe0f453c98621db96dfc98c0f768
ArthurPain <o.p.logisticojjmendez@gmail.com>
2023-08-22 09:53:19 -0500

    feat

--- 13-Metodos-Strings/metodosStrings.js
+++ 13-Metodos-Strings/metodosStrings.js
@@ -18,3 +18,25 @@ Devuelve un fragmento de una cadena de texto.
 2do parametro (opcional) - Index hasta donde queremos cortar.
 */
 console.log(text.slice(4, 12));
+
+/* .replace()
+Devuelve una cadena de texto en donde remplaza un valor por otro.
+
+1er parametro - El texto que queremos remplazar.
+
+2do parametro - El texto que queremos poner.
+*/
+const userGreeting = "Good Night Son of Bitches";
+console.log(userGreeting.replace("Night", "Evening"));
+
+/* .split()
+Convierte una cadena de texto en un arreglo.
+
+1er parametro - El caracter que funcionara como separador.
+*/
+const userQuote = "follow your dreams, keep sleeping";
+console.log(userQuote.split("-"));
+
+/* toUpperCase() - toLowerCase() 
+Transforma la cadena a mayusculas - Transforma la cadena a minusculas.
+*/
