febc322a32087c1c72d09c648e8c84bb01b225fb
ArthurPain <o.p.logisticojjmendez@gmail.com>
2023-09-05 19:50:25 -0500

    update

--- 021-Modulos/public/bundle.js
+++ 021-Modulos/public/bundle.js
@@ -1,14 +1,2 @@
 'use strict';
 
-/*
-Cuando solamente queremos exportar una función:
- */
-const getUser = () => {
-  return {
-    name: "Juana",
-    email: "juan@gmail.com",
-  };
-};
-
-/* Cuando se vaya a import no necesitaremos llaves y podremos llamarla como queramos */
-console.log(getUser());
