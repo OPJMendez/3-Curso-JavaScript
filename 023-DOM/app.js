a92211c3ab7db56a4b5f740a2dc3d521caa1cf2c
ArthurPain <o.p.logisticojjmendez@gmail.com>
2023-10-06 15:10:57 -0500

    feat

--- 023-DOM/app.js
+++ 023-DOM/app.js
@@ -1,34 +1,10 @@
-const addBox = () => {
-  /* Primer paso: Creamos el elemento
-        
-        createElement - Recibe como parametro una cadena de texto con la etiqueta que queremos crear.
-        */
-
-  const newSection = document.createElement('section');
-
-  /* Segundo paso: Agregamos contenido y atributos */
-
-  newSection.innerText = 'Nueva Caja';
-  newSection.setAttribute('id', 'nuevo-id');
-  newSection.setAttribute('class', 'caja activa');
-
-  /* Agregamos el elemento al DOM */
-  const mainContainer = document.getElementById('contenedor1');
-
-  /* .insertAdjacentElement()
-  
-  Nos permite agregar un elemento.
-
-  Valores que recibe:
-
-  afterbegin - como primer elemento (como primer elemento del padre)
-
-  beforebegin - antes del elemento padre (antes del elemento padre)
-
-  beforeend - como ultimo elemento (como ultimo elemento del elemento padre)
-
-  afterend - despues del elemento padre (despues del elemento padre)
-  */
-
-  mainContainer.insertAdjacentElement('afterbegin', newSection);
+const deleteBox = () => {
+  /* Eliminar elementos */
+  const container = document.getElementById('contenedor1');
+  const boxToDelete = container.querySelector('.caja');
+  if (boxToDelete) {
+    container.removeChild(boxToDelete);
+  } else {
+    console.log('theres no more box to delete');
+  }
 };
