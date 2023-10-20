ccea46d98dca6a30dabed7f7f62dbe56a1887881
ArthurPain <o.p.logisticojjmendez@gmail.com>
2023-10-20 13:37:02 -0500

    feat

--- 024-Formularios/app.js
+++ 024-Formularios/app.js
@@ -1,8 +1,51 @@
-const formulario = document.forms['formulario-donacion'];
+const formulario = document.getElementById('formulario-donacion');
 
-formulario.correo.addEventListener('keydown', (event) => {
-  console.log(`La tecla: ${event.key} fue presionada`);
-});
-formulario.correo.addEventListener('keyup', (event) => {
-  console.log(`La tecla: ${event.key} dejo de ser presionada`);
+formulario.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Obtenemos los datos
+  const userData = {
+    correo: formulario.correo.value,
+    pais: formulario.pais.value,
+    donacion: formulario.donacion.value,
+    fecha: formulario.fecha.value,
+    TyC: formulario['terminos-y-condiciones'].checked,
+  };
+
+  // validamos el correo
+  if (userData.correo.length <= 2) {
+    console.log(`Correo Invalido: ${userData.correo}`);
+
+    return;
+  }
+
+  // validamos el pais
+  if (userData.pais === '') {
+    console.log('Pais Invalido');
+
+    return;
+  }
+
+  // validamos la donación
+  if (userData.donacion === '') {
+    console.log('Seleccione una cantidad');
+
+    return;
+  }
+
+  /* Validamos la fecha */
+  if (userData.fecha === '') {
+    console.log('Selecciona una fecha correcta');
+
+    return;
+  }
+
+  /* Validamos TyC */
+  if (!userData.TyC) {
+    console.log('Debe aceptar los terminos y condiciones');
+
+    return;
+  }
+  console.log(userData);
+  console.log('Enviando datos...');
 });
